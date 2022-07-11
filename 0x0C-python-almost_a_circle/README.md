c86cbf368571b116281162265a5e8bae91ea8b2e
root <p.amoako2002@gmail.com>
2022-07-11 15:57:58 -0700

    python

--- 0x0C-python-almost_a_circle/README.md
+++ 0x0C-python-almost_a_circle/README.md
@@ -1,1 +1,4 @@
-# 0x0C. Python - Almost a circle
+# Python - Almost a circle
+
+
+In this project, I expressed my skills in Python object-oriented programming by coding three connected classes to represent rectangles and squares. I wrote my own test suite using the `unittest` module to test all functionality for each class.
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,51 +1,83 @@
 #!/usr/bin/python3
-"""Module for the class Rectangle"""
-
-
+"""Defines a square class."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Class Base inherits from Rectangle"""
+    """Represent a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Constructor of square"""
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """property of size"""
+        """Get/set the size of the Square."""
         return self.width
 
     @size.setter
     def size(self, value):
-        """setter of size but unsing the attribute of Rectangle:
-        - first width because we want the error validation of its"""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """refresh the values of square attributes"""
-        args_list = ["id", "width", "x", "y"]
-        if args and args[0] is not None:
-            if len(args) > len(args_list):
-                max_len = len(args_list)
-            else:
-                max_len = len(args)
-            for i in range(max_len):
-                setattr(self, args_list[i], args[i])
-        elif kwargs is not None:
-            for key in kwargs:
-                setattr(self, key, kwargs[key])
+        """Update the Square.
 
-    def __str__(self):
-        """overriding to return [Square] + more information"""
-        string = "[Square] ({:d}) {:d}/{:d} - {:d}"
-        string = string.format(self.id, self.x, self.y, self.width)
-        return string
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
-        """Method that returns the dictionary representation of a Rectangle"""
-        key_list = ["id", "size", "x", "y"]
-        value_list = [self.id, self.width, self.x, self.y]
-        return dict(zip(key_list, value_list))
+        """Return the dictionary representation of the Square."""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
